b14be511d06952053550e624cc57fa37abb96e92
naman kumar singh <singhnaman320@gmail.com>
2022-11-05 13:31:13 +0530

    updated portfolio

--- main.js
+++ main.js
@@ -23,18 +23,18 @@ closemenu=()=>{
     sidemenu.style.right="-200px";
 }
 
-//   const scriptURL = ''
-//   const form = document.forms['submit-to-google-sheet']
-//   const msg=document.getElementById("msg");
-//   form.addEventListener('submit', e => {
-//     e.preventDefault()
-//     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-//       .then(response => {
-//         msg.innerHTML="Message Sent Successfully";
-//         setTimeout(()=>{
-//             msg.innerHTML="";
-//         },5000);
-//         form.reset();
-//     })
-//       .catch(error => console.error('Error!', error.message))
-//   })
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwotSquEReIcg899fxVdOaOCaplhjnh0suOXSRouMn5kc6w-DpPDmOk13bhkY6PnkQ5pQ/exec';
+  const form = document.forms['submit-to-google-sheet']
+  const msg=document.getElementById("msg");
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML="Message Sent Successfully";
+        setTimeout(()=>{
+            msg.innerHTML="";
+        },5000);
+        form.reset();
+    })
+      .catch(error => console.error('Error!', error.message))
+  })
