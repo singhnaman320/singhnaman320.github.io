7f46dfa57961eb4e4ee7ce494d1d56518947cfcb
naman kumar singh <singhnaman320@gmail.com>
2023-04-28 14:25:23 +0530

    Modification in about section

--- main.js
+++ main.js
@@ -49,7 +49,9 @@ closemenu=()=>{
     document.getElementById("resume-button-2").addEventListener("click",resume1_fun);
     function resume1_fun() {
     //window.location.href = "https://drive.google.com/file/d/1_kuCzH24NVL_wiE3UkilIi1mXk2Y0Yw2/view?usp=share_link";
-    window.open("https://1drv.ms/b/s!AjVEmfDOaqo9hiFqnTQmr9w8u1rb?e=3yeSZ7", '_blank');
+    //window.open("https://1drv.ms/b/s!AjVEmfDOaqo9hiFqnTQmr9w8u1rb?e=3yeSZ7", '_blank');
+
+    resume_fun();
     
     }
 
